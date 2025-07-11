5a9ea7c619648e0be9f2b2457ff91ad6096e8c22
firas chiha <medfiraschiha@gmail.com>
2025-07-11 16:45:01 +0200

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/signup";
+import ForGotPassword from "./pages/ForGotPassword";
 function App() {
   return (
     <>
-      <h1> hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForGotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/ForGotPassword.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ForGotPassword() {
+  return (
+    <div>ForGotPassword</div>
+  )
+}
